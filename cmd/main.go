a654793c267dc8b37c667fea2adf6c637a3199b6
Dimitrius <admin@Admins-MacBook-Pro.local>
2022-06-12 01:26:34 +0700

    somethink wrong with importing variable from env?

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,49 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/Includeoyasi/todo-app"
 	"github.com/Includeoyasi/todo-app/pkg/handler"
+	"github.com/Includeoyasi/todo-app/pkg/repository"
+	"github.com/Includeoyasi/todo-app/pkg/service"
+	"github.com/joho/godotenv"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := InitConfig(); err != nil {
+		log.Fatalf("Error initializing config: %s", err.Error())
+	}
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error to loding env var: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+	if err != nil {
+		log.Fatalf("Failed to initialize db: %s", err.Error())
+	}
+
+	repository := repository.NewRepository(db)
+	service := service.NewService(repository)
+	handler := handler.NewHandler(service)
+
 	srv := new(todo.Server)
-	handlers := new(handler.Handler)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handler.InitRoutes()); err != nil {
 		log.Fatalf("Error while running http server %s", err.Error())
 	}
+}
+
+func InitConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,8 +1,18 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/Includeoyasi/todo-app/pkg/service"
+	"github.com/gin-gonic/gin"
+)
 
 type Handler struct {
+	service *service.Service
+}
+
+func NewHandler(s *service.Service) *Handler {
+	return &Handler{
+		service: s,
+	}
 }
 
 func (h *Handler) InitRoutes() *gin.Engine {
